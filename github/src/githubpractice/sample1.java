68e217ea580d13aad1195381e5f74413304c9698
Welcome <Welcome@Welcome-PC>
2017-06-29 13:54:44 +0530

    onelinechange

--- github/src/githubpractice/sample1.java
+++ github/src/githubpractice/sample1.java
@@ -2,6 +2,9 @@ package githubpractice;
 
 public class sample1 {
 public static void main(String[] args) {
+System.out.println("hello github,thank you");
+	
 	System.out.println("hello world");
 }
+
 }
