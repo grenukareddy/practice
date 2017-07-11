eeae20cab18b68beb2032e01d3867466da0857e1
grenukareddy <renukareddy240@gmail.com>
2017-07-11 15:03:49 +0530

    Update sample1.java
    changes in master

--- github/src/githubpractice/sample1.java
+++ github/src/githubpractice/sample1.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 System.out.println("hello github,thank you");
 	System.out.println("love");
 	System.out.println("hello world");
+	System.out.println("new changesin master");
 }
 
 }
